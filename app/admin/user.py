2e4c628e83301815720516a8446b1b64816603f0
TianaNanta <andrianjakananta@gmail.com>
2024-05-22 20:58:26 +0300

    Add some configuration for user_admin

--- app/admin/user.py
+++ app/admin/user.py
@@ -4,4 +4,12 @@ from app.models import User
 
 
 class UserAdmin(ModelView, model=User):
-    column_list = [User.id, User.email]
+    column_list = [User.id, User.email, User.full_name]
+    column_searchable_list = [User.email]
+    column_sortable_list = [User.id]
+    column_default_sort = [(User.email, True), (User.full_name, False)]
+    column_details_exclude_list = [User.hashed_password, User.items]
+
+    # pagination configuration
+    page_size = 50
+    page_size_options = [25, 50, 100, 200]
