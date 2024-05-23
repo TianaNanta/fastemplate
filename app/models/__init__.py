09ba3ca13de7f795ac308a032066e1463f3dfa88
Restyled.io <commits@restyled.io>
2024-05-23 02:19:08 +0000

    Restyled by isort

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,17 +2,6 @@ from sqlmodel import SQLModel  # noqa: F401
 
 from .base import TimeStampedModel
 from .item import Item, ItemBase, ItemCreate, ItemPublic, ItemUpdate
-from .user import (
-    Message,
-    NewPassword,
-    Token,
-    TokenPayload,
-    UpdatePassword,
-    User,
-    UserBase,
-    UserCreate,
-    UserPublic,
-    UserRegister,
-    UserUpdate,
-    UserUpdateMe,
-)
+from .user import (Message, NewPassword, Token, TokenPayload, UpdatePassword,
+                   User, UserBase, UserCreate, UserPublic, UserRegister,
+                   UserUpdate, UserUpdateMe)
