256cf82d0d77c040e62a836b17b339709206e4d7
Restyled.io <commits@restyled.io>
2024-05-23 04:46:52 +0000

    Restyled by black

--- app/models/item.py
+++ app/models/item.py
@@ -10,6 +10,7 @@ from app.models import SQLModel
 # Shared properties
 class ItemBase(SQLModel):
     """ """
+
     title: str = Field(index=True)
     description: str | None = None
 
@@ -17,23 +18,24 @@ class ItemBase(SQLModel):
 # Properties to receive on item creation
 class ItemCreate(ItemBase):
     """ """
+
     title: str
 
 
 # Properties to receive on item update
 class ItemUpdate(ItemBase):
     """ """
+
     title: str | None = None  # type: ignore
 
 
 # Database model, database table inferred from class name
 class Item(ItemBase, TimeStampedModel, table=True):
     """ """
+
     id: int | None = Field(default=None, primary_key=True)
     title: str
-    owner_id: int | None = Field(default=None,
-                                 foreign_key="user.id",
-                                 nullable=False)
+    owner_id: int | None = Field(default=None, foreign_key="user.id", nullable=False)
     owner: User | None = Relationship(back_populates="items")
 
     def __str__(self):
@@ -43,6 +45,7 @@ class Item(ItemBase, TimeStampedModel, table=True):
 # Properties to return via API, id is always required
 class ItemPublic(ItemBase, TimeStampedModel):
     """ """
+
     id: int
     owner_id: int
     owner: UserPublic
